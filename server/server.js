aae27070dae4e1536f595a370735a2a9ca960415
Kaurgurjot <35602778+Kaurgurjot@users.noreply.github.com>
2018-08-10 16:27:54 -0400

    IP changes

--- server/server.js
+++ server/server.js
@@ -16,20 +16,24 @@ app.post('/api/*', (req, res) => {
 
 	
 	request.post({
-		'headers': { 'content-type': 'application/json' ,
-		'User-Agent': 'Request-Promise',
-		'Authorization': 'Basic '+  'gbandasha:Nish@123'},
-		'url': req.body.url,
-		'body': JSON.stringify(req.body.content)
-	}, (error, response, body) => {
-		if(error) {
-			return console.dir(error);
-		}
-		console.dir('Response Body - ' + JSON.stringify(body));
-		
-		res.setHeader('Content-Type', 'application/json');
-	    res.send(body);
-	});
+        "headers": { "content-type": "application/json" },
+        "url": req.body.url,
+        "body": JSON.stringify(req.body.content),
+        "auth": {
+            "user": "gbandasha",
+            "pass": "Nish@123",
+            "sendImmediately": true
+          }
+    }, (error, response, body) => {
+        if(error) {
+            return console.dir(error);
+        }
+        console.dir('Response Body - ' + JSON.stringify(body));
+        
+        res.setHeader('Content-Type', 'application/json');
+        res.send(body);
+    });
+
 	
 	
 
